b2b2fcbd8f0a37ba3cad9569ed0a685dae805638
VitorLima <vitorgl@ciandt.com>
2023-09-10 23:50:30 -0300

    Criando Connection Factory para conexoes do banco

--- src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -1,28 +1,17 @@
 package br.com.alura.bytebank;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-
     public Connection recuperarConexao() {
         try {
-            return createDataSource().getConnection();
+            return DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=sua_nova_senha");
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private HikariDataSource createDataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:mysql://localhost:3306/byte_bank");
-        config.setUsername("root");
-        config.setPassword("root");
-        config.setMaximumPoolSize(10);
-
-        return new HikariDataSource(config);
     }
 }
